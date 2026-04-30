7e71a3a1527b80347fcd66eb2196eb6c22e7e248
Antonio Cortés <anntoniocorteslopez@gmail.com>
2026-04-30 16:51:38 +0200

    kubernetes info added

---
+++ components/k8s/instalacion-configuracion/instalacion-configuracion.js
@@ -0,0 +1,7 @@
+import { createComponent } from "https://antoniocortes.github.io/base-webpage/js/component-generator.js";
+
+const tagName = 'component-k8s-instalacion-configuracion';
+const htmlFilename = 'instalacion-configuracion.html';
+
+const baseUrl = import.meta.url.substring(0, import.meta.url.lastIndexOf('/') + 1);
+createComponent(tagName, baseUrl + htmlFilename);
